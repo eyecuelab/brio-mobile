4dd84d69080e21f331483de659e649e826ee1b04
Kevin <thekidnamedkd@gmail.com>
2020-11-02 11:32:55 -0800

    file add

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, Button, TextInput } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <PaperProvider>
       <StatusBar style="auto" />
+    <View style={styles.container}>
+      <View>
+        <View>
+         <Text>Hello, friend</Text>
+        </View>
+      </View>
+
+      <TextInput
+        label="Email..."
+        style={styles.input}
+        mode="flat"
+        underlineColor="tomato"
+      ></TextInput>
+      <TextInput
+        label="Password..."
+        style={styles.input}
+        mode="flat"
+        underlineColor="white"
+      ></TextInput>
+
+      <Button 
+        onPress={() => console.log("HI KEVIN")}
+        mode="contained"
+        style={styles.button}
+      >LOGIN</Button>
+      <Button 
+        onPress={() => console.log("HI CHEE")}
+        mode="contained"
+        style={styles.button}
+      >REGISTER</Button>
+      <Text>Forgot password?</Text>
     </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    marginTop: 30,
+    backgroundColor: 'lightgreen',
+  },
+  input: {
+    marginTop: 30,    
+  }
+
 });
