280579b04e0a6f73cb25892527943a5a4def1a2a
Kevin <thekidnamedkd@gmail.com>
2020-11-03 17:36:33 -0800

    working login button with nav

--- src/screens/Login.Page.js
+++ src/screens/Login.Page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import LoginButton from "./Login.Button.js";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { TextInput } from "react-native-paper";
 import styles from "./Login.Button.Style.js";
 import text from "./Text.Style.js";
@@ -8,7 +8,7 @@ import bg from "./Screen.Style.js"
 import input from "./TextInput.Style.js";
 
 
-export const LoginPage = () => {
+export const LoginPage = ({ navigation }) => {
   return (
     <>
       <View style={bg.container}>
@@ -26,7 +26,13 @@ export const LoginPage = () => {
           underlineColor="black"
         ></TextInput>
 
-        <LoginButton />
+        {/* <LoginButton /> */}
+
+        <Button
+          title="Login"
+          onPress={() => navigation.navigate('Dash')}
+        />
+        
 
         <Text style={text.text}>Sign up for an account?</Text>
         <Text style={text.text}>Forgot password?</Text>
