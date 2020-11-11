4b63bcfb63b5764965484092c241c877bac7a12e
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-11 12:37:15 -0800

    refactor LoginPage and SignupPage components using ES6 and change exporting name. Use useState to store input email and password, will make logIn function in detail creating logInWatcher.

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -1,57 +1,80 @@
-import React from "react";
+// REACT, REACT NATIVE //
+import React, { useState, useRef } from "react";
 import { Text, View, TouchableOpacity, Image } from "react-native";
+import { connect } from "react-redux";
+import * as actions from "../rdx/actions";
+import { useNavigation } from "@react-navigation/native";
+// STYLES //
 import { TextInput } from "react-native-paper";
 import text from "../styles/TextStyle.js";
 import bg from "../styles/ScreenStyle.js";
 import input from "../styles/TextInputStyle.js";
-import login from "../styles/LoginButtonStyle.js"
+import login from "../styles/LoginButtonStyle.js";
 import brio from "../../assets/Brio_Star.png";
 
-export const LoginPage = ({ navigation }) => {
+const LoginPage = (props) => {
+  const navigation = useNavigation();
+  const logInWatcher = props.logInWatcher;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const inputEmail = useRef(null);
+  const inputPassword = useRef(null);
+
+  const logIn = () => {
+    //navigation.navigate('Dash')
+    //logInWatcher({});
+    console.log("LOGIN PRESSED")
+  };
   return (
     <>
       <View style={bg.brick}>
         <Text style={text.header}>Hello, friend...</Text>
         <View>
-               <Image source={brio} />
+          <Image source={brio} />
         </View>
         <TextInput
           label="Email..."
           mode="flat"
           underlineColor="black"
           style={input.container}
+          ref={inputEmail}
+          returnKeyType="next"
+          autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
+          onSubmitEditing={() => inputPassword.current.focus()}
         ></TextInput>
         <TextInput
           label="Password..."
           mode="flat"
           style={input.container}
           underlineColor="black"
+          ref={inputPassword}
+          returnKeyType="next"
+          autoCapitalize="none"
+          value={password}
+          onChangeText={setPassword}
+          onSubmitEditing={() => inputConfirmPassword.current.focus()}
         ></TextInput>
 
-        <TouchableOpacity
-          style={login.container}
-          onPress={() => navigation.navigate('Dash')}
-        >
-          <Text>
-            Login
-          </Text>
+        <TouchableOpacity style={login.container} onPress={() => logIn()}>
+          <Text>Login</Text>
         </TouchableOpacity>
-        
-        <Text 
-          onPress={() => navigation.navigate('Signup')}
-          style={text.text}
-        >
-            Sign up for an account?
+
+        <Text onPress={() => navigation.navigate("Signup")} style={text.text}>
+          Sign up for an account?
         </Text>
-        <Text
-          onPress={() => navigation.navigate('Forgot')}
-          style={text.text}
-        >
+        <Text onPress={() => navigation.navigate("Forgot")} style={text.text}>
           Forgot password?
         </Text>
       </View>
     </>
   );
 };
-
-export default LoginPage;
+const mapStateToProps = () => {
+  return {};
+};
+const LoginPageConnected = connect(mapStateToProps, actions)(LoginPage);
+export default LoginPageConnected;
