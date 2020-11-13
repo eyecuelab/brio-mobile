365f6916e7b99bb12b3c0a0b407a75573341123b
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-13 15:14:24 -0800

    add styling on Category and CategoryList

--- src/screens/onboard/categories/CategoryList.js
+++ src/screens/onboard/categories/CategoryList.js
@@ -1,10 +1,31 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import text from "../../../styles/TextStyle.js";
+import bg from "../../../styles/ScreenStyle.js";
+import brio from "../../../../assets/Brio_Star.png";
 
 function CategoryList() {
   return (
-    <View>
-      <Text>HELLO THESE ARE THE LISTS</Text>
+    <View style={bg.lime}>
+      <View>
+        <Image source={brio} />
+      </View>
+      <View>
+        <Text style={text.text}>
+          Here's a few categories we can generate ideas from to stay motivated:
+        </Text>
+      </View>
+      <View>
+      </View>
+        <Text style={text.text}>
+         Conversation
+        </Text>
+        <Text style={text.text}>
+         Music
+        </Text>
+        <Text style={text.text}>
+         Exercise
+        </Text>
     </View>
   );
 }
