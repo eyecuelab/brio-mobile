e3366306c17c7f307b54e1d59a50e6f637f49821
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-13 13:53:37 -0800

    add material-top-tabs for tab navigation

--- src/screens/onboard/categories/CategoryList.js
+++ src/screens/onboard/categories/CategoryList.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from "react";
 import { View, Text } from "react-native";
 
 function CategoryList() {
   return (
     <View>
-    <Text>
-      HELLO THESE ARE THE LISTS
-    </Text>
-  </View>
-  )
+      <Text>HELLO THESE ARE THE LISTS</Text>
+    </View>
+  );
 }
 
-export default CategoryList
+export default CategoryList;
