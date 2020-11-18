18d004c4cef157a24c34a6164b28b4e9914bd100
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-18 11:24:42 -0800

    set up redux-persist in store.js

--- src/rdx/store/index.js
+++ src/rdx/store/index.js
@@ -1,25 +1,23 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import createSagaMiddleware from "redux-saga";
-// import { persistStore, persistReducer } from 'redux-persist';
-// import AsyncStorage from '@react-native-community/async-storage';
+import { persistStore, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-community/async-storage";
+// Saga
 import rootReducer from "../reducers";
+import createSagaMiddleware from "redux-saga";
 import rootSaga from "../sagas";
 
-// const persistConfig = {
-//   key: 'root',
-//   storage: AsyncStorage,
-// };
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(persistedReducer, reduxDevTools(middleware));
+let persistor = persistStore(store);
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+// Saga
 const sagaMiddleware = createSagaMiddleware();
 let middleware = applyMiddleware(sagaMiddleware);
-const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(rootReducer, reduxDevTools(middleware));
-// let persistor = persistStore(store);
-
 sagaMiddleware.run(rootSaga);
 
-export {
-  store,
-  // persistor
-};
+export { store, persistor };
--- src/screens/auth/LoginPage.js
+++ src/screens/auth/LoginPage.js
@@ -1,20 +1,20 @@
 // REACT, REACT NATIVE //
-import React, { useState, useEffect } from "react";
-import { Text, View, Image, ScrollView, Button } from "react-native";
+import React, { useEffect } from "react";
+import { Text, View, Image, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 // STYLES //
 import text from "../../styles/TextStyle.js";
 import bg from "../../styles/ScreenStyle.js";
 import brio from "../../../assets/Brio_Star.png";
 // EXPO AUTH
-import * as WebBrowser from 'expo-web-browser';
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
-import { SPOTIFY_CLIENT_ID } from '@env';
+import * as WebBrowser from "expo-web-browser";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { SPOTIFY_CLIENT_ID } from "@env";
 
 WebBrowser.maybeCompleteAuthSession();
 const discovery = {
-  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
-  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+  authorizationEndpoint: "https://accounts.spotify.com/authorize",
+  tokenEndpoint: "https://accounts.spotify.com/api/token",
 };
 
 const LoginPage = () => {
@@ -22,36 +22,41 @@ const LoginPage = () => {
   const [request, response, promptAsync] = useAuthRequest(
     {
       clientId: SPOTIFY_CLIENT_ID,
-      scopes: ['user-read-email', 'playlist-modify-public'],
+      scopes: ["user-read-email", "playlist-modify-public"],
       usePKCE: false,
       redirectUri: makeRedirectUri({
-        native: 'brio-mobile://redirect',
+        native: "brio-mobile://redirect",
       }),
     },
     discovery
   );
 
   useEffect(() => {
-    if (response?.type === 'success') {
+    if (response?.type === "success") {
+      //set this code in to store
       const { code } = response.params;
       navigation.navigate("CategoryNavigation");
-      }
+    }
   }, [response]);
 
+  
   return (
     <>
-        <View style={bg.brick}>
-          <Text style={text.header}>Hello, friend...</Text>
-            <Image source={brio} />
-          <Text style={text.text}>Brio uses Spotify's built-in credentials to access the app. You will need to make an account before proceeding.</Text>
-          <Button
-            disabled={!request}
-            title="Login"
-            onPress={() => {
-              promptAsync();
-              }}
-          />
-        </View>
+      <View style={bg.brick}>
+        <Text style={text.header}>Hello, friend...</Text>
+        <Image source={brio} />
+        <Text style={text.text}>
+          Brio uses Spotify's built-in credentials to access the app. You will
+          need to make an account before proceeding.
+        </Text>
+        <Button
+          disabled={!request}
+          title="Login"
+          onPress={() => {
+            promptAsync();
+          }}
+        />
+      </View>
     </>
   );
 };
