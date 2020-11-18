7c21e3e85157a8215c71c074733a145c2026602b
Kevin <thekidnamedkd@gmail.com>
2020-11-18 11:34:52 -0800

    modify padding for screen style

--- src/styles/ScreenStyle.js
+++ src/styles/ScreenStyle.js
@@ -3,42 +3,42 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   brick: {
     flex: 1,
-    padding: 12,
+    padding: 8,
     backgroundColor: "#FFDFDF",
   },
   citrus: {
     flex: 1,
-    padding: 12,
+    padding: 8,
     backgroundColor: "#FFF0DF",
   },
   sun: {
     flex: 1,
-    padding: 12,
+    padding: 8,
     backgroundColor: "#FFFEDF",
   },
   lime: {
     flex: 1,
-    padding: 12,
+    padding: 8,
     backgroundColor: "#F1FFDF",
   },
   mint: {
     flex: 1,
-    padding: 12,
+    padding: 8,
     backgroundColor: "#DFFFEF",
   },
   robin: {
     flex: 1,
-    padding: 12,
+    padding: 8,
     backgroundColor: "#DFFBFF",
   },
   grape: {
     flex: 1,
-    padding: 12,
+    padding: 8,
     backgroundColor: "#DFEAFF",
   },
   berry: {
     flex: 1,
-    padding: 12,
+    padding: 8,
     backgroundColor: "#FFDFFE",
   },
 });
