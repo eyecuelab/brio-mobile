cd4be392980fdeafb416eca17ac9efab8a18d26c
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-23 14:46:35 -0800

    temporally added blocker navigations at bottom bar in dash

--- src/screens/standard/StandardNavigation.js
+++ src/screens/standard/StandardNavigation.js
@@ -2,7 +2,9 @@ import * as React from "react";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import DashboardPage from "./DashboardPage";
-import ConvoCheck from "./ConvoCheck";
+import BlockerConvo from "../onboard/blockers/BlockerConvo";
+import BlockerExercise from "../onboard/blockers/BlockerExercise";
+import BlockerMusic from "../onboard/blockers/BlockerMusic";
 import Profile from "../auth/Profile";
 
 const Tab = createMaterialBottomTabNavigator();
@@ -28,10 +30,30 @@ export default function StandardNavigation() {
         />
 
         <Tab.Screen
-          name="conveCheck"
-          component={ConvoCheck}
+          name="BlockerConvo"
+          component={BlockerConvo}
           options={{
-            tabBarLabel: "Category",
+            tabBarLabel: "Conversation",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="group" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="BlockerExercise"
+          component={BlockerExercise}
+          options={{
+            tabBarLabel: "Conversation",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="group" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="BlockerMusic"
+          component={BlockerMusic}
+          options={{
+            tabBarLabel: "Conversation",
             tabBarIcon: ({ color }) => (
               <MaterialCommunityIcons name="group" color={color} size={26} />
             ),
