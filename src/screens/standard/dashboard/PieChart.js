2264649c7bf1e89398f9eea8070743e8a21247d1
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-24 16:35:07 -0800

    add sample data and display pie chart

--- src/screens/standard/dashboard/PieChart.js
+++ src/screens/standard/dashboard/PieChart.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { connect } from "react-redux";
+import { VictoryPie } from "victory-native";
+import Svg from "react-native-svg";
 
 export const PieChart = () => {
+  const data = [
+    { x: "Social", y: 35 },
+    { x: "Exercise", y: 40 },
+    { x: "Music", y: 55 },
+  ];
+
   return (
     <>
       <View>
-        <Text>Pie chart</Text>
+        <Svg width={400} height={400}>
+          <VictoryPie
+            standalone={false}
+            innerRadius={75}
+            labelRadius={125}
+            data={data}
+            style={{ labels: { fontSize: 20 } }}
+          />
+        </Svg>
       </View>
     </>
   );
