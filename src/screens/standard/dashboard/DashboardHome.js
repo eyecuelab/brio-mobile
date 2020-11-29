2f728c66f174a204c760b2b5ea7c3e5372ea1fad
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-28 19:31:41 -0800

    add styled components in DashboardHome

--- src/screens/standard/dashboard/DashboardHome.js
+++ src/screens/standard/dashboard/DashboardHome.js
@@ -4,6 +4,7 @@ import Svg from "react-native-svg";
 import { connect } from "react-redux";
 import bg from "../../../styles/ScreenStyle";
 import SvgBrioHome from "../../../svg_assets/SvgBrioHome";
+import styled from "styled-components/native";
 
 export const DashboardHome = (props) => {
   const { allBlockers } = props;
@@ -11,11 +12,38 @@ export const DashboardHome = (props) => {
   return (
     <>
       <View style={bg.basic}>
-        <SvgBrioHome style={{width: 500, height: 500}}/>
+        <DiagramContainer>
+          <SvgBrioHome />
+          <Diagram>
+            <DiagramTextHeader>Oh my gosh!</DiagramTextHeader>
+            <DiagramText>This is going to be su much fun. </DiagramText>
+            <DiagramText>What are we going to do today?</DiagramText>
+          </Diagram>
+        </DiagramContainer>
       </View>
     </>
   );
 };
+
+const DiagramContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 28;
+`;
+const Diagram = styled.View`
+  margin-top: 28;
+`;
+const DiagramTextHeader = styled.Text`
+  font-size: 35px;
+  color: #51ade0;
+  font-weight: 900;
+`;
+const DiagramText = styled.Text`
+  font-size: 18px;
+  color: #51ade0;
+  font-weight: 900;
+`;
 
 const mapStateToProps = (state) => {
   return {
