651ecdfc94f9ddbfe6090957b669c183617b7ac2
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-30 13:36:01 -0800

    implement most recent completed date for exercise and display it in the list in DashboardHome

--- src/screens/standard/dashboard/DashboardHome.js
+++ src/screens/standard/dashboard/DashboardHome.js
@@ -15,6 +15,45 @@ import { List } from "react-native-paper";
 export const DashboardHome = (props) => {
   const { allBlockers } = props;
 
+  const exeBlockers = allBlockers.filter(
+    (blocker) => blocker.category === "exercise"
+  );
+  const sortedExeBlockersByCompletedAt = exeBlockers.sort(function (a, b) {
+    return b.completedAt - a.completedAt;
+  });
+  const mostRecentCompletedExeBlocker = sortedExeBlockersByCompletedAt[0];
+
+  const mostRecentCompletedDateExe = mostRecentCompletedExeBlocker.completedAt;
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const exeMonth = months[mostRecentCompletedDateExe.getMonth()];
+  const exeDay = days[mostRecentCompletedDateExe.getDay()];
+  const exeDate = mostRecentCompletedDateExe.getDate();
+  const exeYear = mostRecentCompletedDateExe.getFullYear();
+
   return (
     <>
       <View style={bg.basic}>
@@ -35,7 +74,7 @@ export const DashboardHome = (props) => {
             fontSize: 36,
             fontWeight: "bold",
           }}
-          description="LAST CHECKIN Sunday Nov 22, 2020"
+          description={`LAST CHECKIN \n ${exeDay} ${exeMonth} ${exeDate}, ${exeYear}`}
           descriptionNumberOfLines={2}
           descriptionStyle={{
             fontFamily: "Avenir-Light",
