df6e97f27441660c7caa48dca1dd1a69e727be40
Kevin <thekidnamedkd@gmail.com>
2020-12-07 15:41:57 -0800

    add svg to briosuccess

--- src/screens/landing/BrioSuccess.js
+++ src/screens/landing/BrioSuccess.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Text, View } from "react-native";
 import bg from "../../styles/ScreenStyle.js";
 import text from "../../styles/TextStyle.js";
+import SvgLetsDoThis from "../../svg_assets/SvgLetsDoThis"
 
 
 function BrioSuccess() {
     return (
         <View style={bg.grape}>
-                <Text style={text.header}>Fourth Page</Text>
+                <SvgLetsDoThis />
         </View>
     )
 }
