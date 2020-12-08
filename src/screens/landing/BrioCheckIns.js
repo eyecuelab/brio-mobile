00fc7243783e90264d446e6b8de1470e2bf17803
Kevin <thekidnamedkd@gmail.com>
2020-12-07 17:16:16 -0800

    get started button briocheckins

--- src/screens/landing/BrioCheckIns.js
+++ src/screens/landing/BrioCheckIns.js
@@ -2,13 +2,32 @@ import React from 'react';
 import { Text, View } from "react-native";
 import bg from "../../styles/ScreenStyle.js";
 import SvgTellMeWhen from "../../svg_assets/SvgTellMeWhen";
-
+import styled from "styled-components/native";
+import GetStartedBtn from "./GetStartedBtn";
 
 function BrioCheckIns() {
     return (
         <View style={bg.basic}>
-                <SvgTellMeWhen />
+                <DiagramContainer>
+                    <Diagram>
+                        <SvgTellMeWhen />
+                    </Diagram>
+                </DiagramContainer>
+                <GetStartedBtn text="Get Started" textColor="#ffffff" backgroundColor="#94D7B5" />
         </View>
     )
 }
+
+const DiagramContainer = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-top: 36px;
+`;
+const Diagram = styled.View`
+    margin-top: 36px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`;
 export default BrioCheckIns
