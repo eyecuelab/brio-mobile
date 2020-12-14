55e71e3a1decc3249a06d2e02d1f6981ea9948de
Kevin <thekidnamedkd@gmail.com>
2020-12-14 14:13:48 -0800

    add green slider to intro

--- src/screens/landing/BrioSuggestions.js
+++ src/screens/landing/BrioSuggestions.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React from "react";
 import { View } from "react-native";
 import bg from "../../styles/ScreenStyle.js";
-import SvgSomethingFun from "../../svg_assets/landing/SvgSomethingFun"
+import SvgSomethingFun from "../../svg_assets/landing/SvgSomethingFun";
 import styled from "styled-components/native";
 import GetStartedBtn from "./GetStartedBtn";
+import SvgDotSliderBlue from "../../svg_assets/landing/SvgDotSliderBlue.js";
 
 function BrioSuggestions() {
-    return (
-        <View style={bg.basic}>
-                <DiagramContainer>
-                    <Diagram>
-                        <SvgSomethingFun />
-                    </Diagram>
-                </DiagramContainer>
-                <GetStartedBtn text="Get Started" textColor="#ffffff" backgroundColor="#51ADE0" />
-        </View>
-    )
+  return (
+    <View style={bg.basic}>
+      <DiagramContainer>
+        <Diagram>
+          <SvgSomethingFun />
+        </Diagram>
+      </DiagramContainer>
+      <GetStartedBtn
+        text="Get Started"
+        textColor="#ffffff"
+        backgroundColor="#51ADE0"
+      />
+    </View>
+  );
 }
 
 const DiagramContainer = styled.View`
-    flex: 1;
-    flex-direction: row;
-    justify-content: space-around;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-around;
 `;
 const Diagram = styled.View`
-    margin-top: 36px;
+  margin-top: 36px;
 `;
 
-export default BrioSuggestions
+export default BrioSuggestions;
