2b6de084950621a8a30ba92c72446bdff28f0a38
Kevin <thekidnamedkd@gmail.com>
2020-12-16 14:09:20 -0800

    fix typo for eye color prop

--- src/svg_assets/SvgEyeballPie.js
+++ src/svg_assets/SvgEyeballPie.js
@@ -3,7 +3,7 @@ import { SvgXml } from "react-native-svg";
 export default function SvgEyeballPie(props) {
   const svgMarkup = `<svg width="161" height="161" viewBox="0 0 160 161" fill="none" xmlns="http://www.w3.org/2000/svg">
   <circle cx="80" cy="80.8605" r="80" fill="white"/>
-  <circle cx="73.3333" cy="90.3843" r="34.2857" fill="${props.eyeColor}"/>
+  <circle cx="73.3333" cy="90.3843" r="34.2857" fill=${props.eyeColor} />
   <circle cx="73.3331" cy="90.3843" r="19.0476" fill="#51ADE0"/>
   <circle cx="80.9523" cy="75.1463" r="11.4286" fill="white"/>
   </svg>
