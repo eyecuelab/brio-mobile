67f138b06191b3f94528989ba28087731058862d
Kevin <thekidnamedkd@gmail.com>
2020-12-17 14:45:16 -0800

    add padding to all screens for tab nav

--- src/screens/standard/StandardNavigation.js
+++ src/screens/standard/StandardNavigation.js
@@ -17,7 +17,7 @@ export default function StandardNavigation() {
         activeColor="#757272"
         inactiveColor="#ECC08D"
         barStyle={{
-          backgroundColor: "rgba(255, 255, 255, 0.6)",
+          backgroundColor: "rgba(255, 255, 255, 0.2)",
           position: "absolute",
         }}
         showLabel={false}
